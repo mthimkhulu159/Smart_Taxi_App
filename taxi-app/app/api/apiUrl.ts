73357e6f2267367a6b8039d8bb3b998d4a3217ae
mthimkhulu159 <pertuniamthimkhulu1@gmail.com>
2025-06-16 16:02:03 +0000

    Update app icon and favicon; change API URL to point to the production server

--- taxi-app/app/api/apiUrl.ts
+++ taxi-app/app/api/apiUrl.ts
@@ -1,2 +1,2 @@
-//export const apiUrl = 'https://organic-bassoon-g9rwxvvw94xcw9v6-3000.app.github.dev';
-export const apiUrl = "https://effective-fiesta-7vwpp6g56pw6h96j-3000.app.github.dev"
+export const apiUrl = 'https://qmarshal.onrender.com';
+//export const apiUrl = "https://effective-fiesta-7vwpp6g56pw6h96j-3000.app.github.dev"
