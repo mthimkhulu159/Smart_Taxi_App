a3cee699036261fc4fbf798b26b93128479676aa
mthimkhulu159 <pertuniamthimkhulu1@gmail.com>
2025-06-21 12:13:10 +0000

    fix: Restore original API URL and comment out the alternative

--- taxi-app/app/api/apiUrl.ts
+++ taxi-app/app/api/apiUrl.ts
@@ -1,2 +1,2 @@
-//export const apiUrl = 'https://qmarshal.onrender.com';
-export const apiUrl = "https://effective-fiesta-7vwpp6g56pw6h96j-3000.app.github.dev"
+export const apiUrl = 'https://qmarshal.onrender.com';
+//export const apiUrl = "https://effective-fiesta-7vwpp6g56pw6h96j-3000.app.github.dev"
